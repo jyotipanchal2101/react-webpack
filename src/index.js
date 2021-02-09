fa31d21f9e6bb98f92b292142820535bfd595666
jyotipanchal2101 <jyoti.panchal@cuelogic.com>
2021-02-09 13:58:33 +0530

    created dev branch

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import reactDOM from 'react-dom'
+import './styles/style.css'
+//const template = React.createElement('hi',null,'Hello')
+//const template = <h1>Hello World</h1>
+
+const App = () => {
+    return <h1>Hello from </h1>
+}
+
+reactDOM.render(<App/>,document.getElementById("root"))
